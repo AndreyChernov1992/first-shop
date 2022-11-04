58101293b3c0cc4afd8a9286baa3478d8f5569d0
Coldsun <robotsdontcry92@gmail.com>
2022-11-04 13:45:10 +0700

    Fixed

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_PRODUCTS,
+    baseURL: process.env.REACT_APP_BASE_URL,
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
