800e2ddc13771cb4d860be166dffd0da53894f81
Coldsun <robotsdontcry92@gmail.com>
2022-11-05 15:18:57 +0700

    Fixed

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -11,9 +11,7 @@ const axiosClient = axios.create({
   axiosClient.interceptors.response.use(
     response => response, 
     error => {const {error} = response;
-      if (error.status == 401) {
-        window.location.href = process.env.REACT_APP_BASE_URL;
-      }
+      if (error.status == 401) {window.location.href = process.env.REACT_APP_BASE_URL;}
       console.error("Looks like there was a problem. Status Code: " + res.status);
       return Promise.reject(error);
     }
