9bf56949478553d2d56bcbfda965b80d29735d9a
Coldsun <robotsdontcry92@gmail.com>
2022-11-14 19:35:12 +0700

    Fixed imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import './assets/styles/index.css';
 import App from './App';
-// import Products from './components/products';
-import Routess from './routes/routes';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
