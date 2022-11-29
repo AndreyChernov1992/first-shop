51ff648f7985b00928bc075616b3c3709d2df45d
Coldsun <robotsdontcry92@gmail.com>
2022-11-29 14:35:37 +0700

    Fixed

--- src/services/products.js
+++ src/services/products.js
@@ -2,8 +2,8 @@ import ProductsApi from "../api/productsApi";
 
 export async function getProductsData () {
     const api = new ProductsApi()
-        const {data} = await api.getProducts()
-        return data
+    const {data} = await api.getProducts()
+    return data
 }
 
 
