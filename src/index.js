cc1707c768d1fedc89a06dff1e57397271dd04dd
Coldsun <robotsdontcry92@gmail.com>
2023-01-20 15:23:41 +0700

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
-import './assets/styles/index.css';
+import './assets/styles/main.scss';
 import App from './App';
 
 
