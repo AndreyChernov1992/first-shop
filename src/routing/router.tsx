d26fbcacaa1816178d3b2054b5a8c79c51e06050
AndreyChernov1992 <robotsdontcry92@gmail.com>
2023-12-08 16:30:20 +0700

    fix

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -7,11 +7,7 @@ import ProductsList from "../components/products";
 function Routing() {
     return (
         <Routes>
-<<<<<<<< HEAD:src/routing/router.jsx
-          <Route exact path="/" element={<ProductsList />} />
-========
           <Route path="/" element={<ProductsList />} />
->>>>>>>> ts-hooks:src/routing/router.tsx
         </Routes>
     )
   }
